d932ce61edaa4be6b9596a2cd6f3c05884b1c9b0
Shantanu Mahale <mahaleshantanu3@gmail.com>
2024-02-15 01:12:09 +0530

    fixing eslint and prettify configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  singleQuote: false,
   trailingComma: 'all',
+  endOfLine: 'auto',
+  quoteProps: 'consistent',
+  semi: true,
+  useTabs: false
 };
---
+++ src/Timer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const Timer: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+      <View>
+        <Text>finally</Text>
+      </View>
+    </View>
+  );
+};
